6f94b86b2b1922d89bcc0e39624ce33d6b79ef95
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-06-02 09:54:55 -0500

    Update demo.py

--- PyTorch-YOLOv3/demo.py
+++ PyTorch-YOLOv3/demo.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_def", type=str, default="config/yolov3-custom.cfg", help="path to model definition file")
     parser.add_argument("--weights_path", type=str, default="yolov3_ckpt_6.pth", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/custom/classes.names", help="path to class label file")
-    parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
+    parser.add_argument("--conf_thres", type=float, default=0.5, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
     parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
